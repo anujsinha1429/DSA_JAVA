aca41a3fde68db2af3a0743f156b339f81c2389a
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-03-29 10:11:03 +0530

    spy number

---
+++ ASSIGNMENT_1/que3.java
@@ -0,0 +1,33 @@
+package ASSIGNMENT_1;
+
+import java.util.Scanner;
+
+public class que3 {
+
+    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter the number N ");
+        int n= sc.nextInt();
+        int m=1;
+        int add=0;
+        while (n>0){
+            n=n%10;
+            m=m*n;
+            add=add+n;
+            n=n/10;
+        }
+
+        if (m==add){
+            System.out.println("the number is spy ");
+
+        }
+        else{
+            System.out.println("the number is not spy ");
+        }
+        sc.close();
+
+
+    }
+    
+}
