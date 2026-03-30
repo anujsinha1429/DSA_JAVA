71a4ec2f650472201a293f1eaacc7c1c964e7437
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-03-30 21:46:15 +0530

    odd no.

---
+++ ASSIGNMENT_1/que6.java
@@ -0,0 +1,27 @@
+package ASSIGNMENT_1;
+
+import java.util.Scanner;
+
+public class que6 {
+    public static boolean isOdd(int n){
+    //     return (n & 1)==1;
+    // }
+    String s= Integer.toString(n);
+    char a=s.charAt(s.length()-1);
+    if (a=='1' || a=='3' || a=='5' || a=='7' || a=='9'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter the no. u want to check odd ");
+        int n = sc.nextInt();
+        System.out.println(isOdd(n));
+        sc.close();
+    }
+    
+}
