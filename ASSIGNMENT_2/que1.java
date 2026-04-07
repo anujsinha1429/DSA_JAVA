1bd790c29e4cd1991ac34bbcadd4f5ed47257e9f
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-04-07 23:07:42 +0530

    added

---
+++ ASSIGNMENT_2/que1.java
@@ -0,0 +1,12 @@
+package ASSIGNMENT_2;
+
+public class que1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = 50;
+        System.out.println("the sum of the numbers is : " + (a+b+c+d+e));
+    }
+}
