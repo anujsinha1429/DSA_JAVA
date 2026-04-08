820b5064a93134e640fc03c59734e41e675e4e79
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-04-08 22:42:16 +0530

    instance variable

--- ASSIGNMENT_2/que1.java
+++ ASSIGNMENT_2/que1.java
@@ -1,12 +1,45 @@
 package ASSIGNMENT_2;
 
-public class que1 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
-        int e = 50;
-        System.out.println("the sum of the numbers is : " + (a+b+c+d+e));
+import java.util.Scanner;
+
+class car{
+        String model;
+        int year;
+    void setdetails(){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter the model of the car :");
+        model=sc.nextLine();
+        System.out.println("enter the year of the car :");
+        year=sc.nextInt();
+        sc.close();
+    }
+    void display(){
+        System.out.println(model + " " + year);
+    }
+}
+   public class que1 {
+   public static void main(String[] args) {
+        car c1=new car();
+        car c2=new car();
+        c1.model="toyota";
+        c1.year=2020;
+        c2.setdetails();
+        c1.display();
+        c2.display();
+        if (c1.year>c2.year){
+            System.out.println(c1.model + " is newer than " + c2.model);
+        }
+        else if (c1.year<c2.year){
+            System.out.println(c2.model + " is newer than " + c1.model);
+        }
+        else{
+            System.out.println("both cars are of the same year");
+        }
+
+        
+
+
+
+   
     }
 }
