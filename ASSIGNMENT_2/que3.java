93704b04f1eadf2422536dff463545e056ea9c10
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-04-09 23:06:40 +0530

    que3

---
+++ ASSIGNMENT_2/que3.java
@@ -0,0 +1,43 @@
+package ASSIGNMENT_2;
+
+public class que3 {
+    public static class book{
+        int bookid;
+        double price;
+        int quantity ;
+        static double totalprice;
+   
+    book(int bookid,double price,int quantity){
+        this.bookid=bookid;
+        this.price=price;
+        this.quantity=quantity;
+        totalprice=totalprice+totalamount();
+    }
+    void display(){
+        System.out.println("book id : " + bookid);
+        System.out.println("price : " + price);
+        System.out.println("quantity : " + quantity);
+
+    }
+     double totalamount(){
+       return price*quantity;
+    }
+
+
+}
+public static void main(String[] args) {
+    book b1=new book(1,400.0,2);
+    book b2=new book(2,300.0,3);
+    book b3=new book(3,500.0,1);
+    book b4=new book(4,200.0,4);
+    book b5=new book(5,150.0,5);
+    b1.display();
+    b2.display();
+    b3.display();
+    b4.display();
+    b5.display();
+    System.out.println("total price of all books : " + book.totalprice);
+}
+
+
+}
