413219f8eebdbfdf5d58506496d3be9ab1b2da29
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-04-10 22:39:25 +0530

    que 5

---
+++ ASSIGNMENT_2/que5.java
@@ -0,0 +1,58 @@
+package ASSIGNMENT_2;
+
+import java.util.Scanner;
+
+abstract class shapeq5 {
+        abstract void area();
+
+    }
+    class circle extends shapeq5{
+        Scanner sc=new Scanner(System.in);
+        double radius;
+            circle(){
+            System.out.println("enter the radius of the circle :");
+            radius=sc.nextDouble();
+        }
+        void area(){
+            System.out.println("calculating area of a circle"+3.14*radius*radius);
+        }
+    }
+    class rectangle extends shapeq5{
+        Scanner sc=new Scanner(System.in);
+        double length, breadth;
+         rectangle(){
+            System.out.println("enter the length of the rectangle :");
+            length=sc.nextDouble();
+            System.out.println("enter the breadth of the rectangle :");
+            breadth=sc.nextDouble();
+        }
+        void area(){
+            System.out.println("calculating area of a rectangle"+length*breadth);
+        }
+    }
+    class triangle extends shapeq5{
+        Scanner sc=new Scanner(System.in);
+        double base, height;
+         triangle(){
+            System.out.println("enter the base of the triangle :");
+            base=sc.nextDouble();
+            System.out.println("enter the height of the triangle :");
+            height=sc.nextDouble();
+        }
+        void area(){
+            System.out.println("calculating area of a triangle"+0.5*base*height);
+        }
+    }
+    public class que5 {
+    public static void main(String[] args) {
+       shapeq5 s=new circle();
+        s.area();
+        s=new rectangle();
+        s.area();
+        s=new triangle();
+        s.area();
+    }
+
+}
+
+
