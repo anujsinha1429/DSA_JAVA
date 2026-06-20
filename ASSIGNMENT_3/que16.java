b6b5c5ae5f1641add63041819e618548abfe78c0
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-06-20 17:12:43 +0530

    ASSIGNMENT_3/que16.java

--- ASSIGNMENT_3/que16.java
+++ ASSIGNMENT_3/que16.java
@@ -1,20 +1,20 @@
 package ASSIGNMENT_3;
 
 public class que16 {
-    public static boolean facb(String s,int n,int l){
-        if (n>=l){
+    public static boolean facb(String s,int st,int l){
+        if (st>=l){
             return true;
         }
-        if (s.charAt(n) !=s.charAt(l)){
+        if (s.charAt(st) !=s.charAt(l)){
             return false ;
         }
-        return facb(s,n+1,l-1);
+        return facb(s,st+1,l-1);
 
     }
     public static void main(String[] args) {
         String a="12321";
-        String s=String.valueOf(a); //to convert int into string 
-        boolean result = facb(s,0,s.length()-1);
+        // String s=String.valueOf(a); //to convert int into string 
+        boolean result = facb(a,0,a.length()-1);
         System.out.println(result);
     }
 }
