c1917fcce5a40134f324cc83086eacc7eebf291b
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-06-21 16:21:33 +0530

    pattern_6.java

--- 01_patterns/pattern_6.java
+++ 01_patterns/pattern_6.java
@@ -1,4 +1,12 @@
-package patterns;
+
+// Given an integer n. You need to recreate the pattern given below for any value of N. 
+// Let's say for N = 5, the pattern should look like as below:
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
 
 public class pattern_6 {
     public void pattern6(int n ){
