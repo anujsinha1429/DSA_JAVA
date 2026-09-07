853477419d9bd640fd94f49d26b44b8446aa2caa
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-09-08 00:17:55 +0530

    insertion_sort.java

---
+++ 04_sorting/insertion_sort.java
@@ -0,0 +1,25 @@
+public class insertion_sort {
+    public int [] sort(int arr[]){
+        for (int i=1;i < arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev >= 0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        
+        insertion_sort s = new insertion_sort();
+        int arr[]={5,4,3,2,1};
+        s.sort(arr);
+        for (int i=0 ; i < arr.length ; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
